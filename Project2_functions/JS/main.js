2f6f0097634dbe0fee2866f4dd730c2d3333d9cd
justman511 <jeffrey432@msn.com>
2025-10-05 23:48:45 +0100

    Project2_function folder commit

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+
+let name  = 'John Doe';
+let profession = 'is a professioner gamer';
+
+function eventHandler() {
+   let conc = name + ' ' + profession;
+   document.getElementById('paraID').innerHTML = conc;
+}
+
+function myFunction () {
+    let sentence = 'I am learing';
+    sentence += ' a lot from this book!';
+    document.getElementById('concatenate').innerHTML = sentence;
+}
